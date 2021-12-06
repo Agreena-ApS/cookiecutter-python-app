9a1ec55c7db8c4e13cd851fed5cd58660b71624a
Erustus Agutu <erustusagutu@gmail.com>
2021-12-06 17:08:52 +0000

    Don't call get_settings upon import

--- {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/core/config/logging.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/core/config/logging.py
@@ -1,26 +1,27 @@
 {% if cookiecutter.settings_management == 'y' %}from {{cookiecutter.app_name}}.core.config.settings import get_settings
 
 {% endif -%}
-logging_config = {
-    "version": 1,
-    "disable_existing_loggers": True,
-    "formatters": {
-        "standard": {
-            "format": "%(levelname)s: [%(name)s:%(funcName)s:%(lineno)s] %(message)s",
+def get_logging_config() -> dict:
+    return {
+        "version": 1,
+        "disable_existing_loggers": True,
+        "formatters": {
+            "standard": {
+                "format": "%(levelname)s: [%(name)s:%(funcName)s:%(lineno)s] %(message)s",
+            },
         },
-    },
-    "handlers": {
-        "console": {
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
-            "formatter": "standard",
+        "handlers": {
+            "console": {
+                "level": "DEBUG",
+                "class": "logging.StreamHandler",
+                "formatter": "standard",
+            },
         },
-    },
-    "loggers": {
-        "{{cookiecutter.app_name}}": {
-            "handlers": ["console"],
-            "propagate": False,
-            "level": {% if cookiecutter.settings_management == 'y' %}"DEBUG" if get_settings().debug is True else "INFO"{% else %}"INFO"{% endif %},
+        "loggers": {
+            "{{cookiecutter.app_name}}": {
+                "handlers": ["console"],
+                "propagate": False,
+                "level": {% if cookiecutter.settings_management == 'y' %}"DEBUG" if get_settings().debug is True else "INFO"{% else %}"INFO"{% endif %},
+            },
         },
-    },
-}
+    }
